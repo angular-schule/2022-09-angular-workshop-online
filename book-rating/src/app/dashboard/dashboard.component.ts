50c6afbb3bf50be4921658fef0d75316d69f9b79
Ferdinand Malcher <ferdinand@malcher.media>
2022-09-14 11:31:37 +0200

    Exkurs: Klasse vs Interface

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -13,3 +13,36 @@ export class DashboardComponent implements OnInit {
   }
 
 }
+
+
+class BookC {
+  rating = 1;
+  constructor(public isbn: string, public title: string) {}
+
+  rateUp() {
+    this.rating++;
+  }
+}
+
+const myBookC = new BookC('123', 'Angular');
+
+
+////////////
+
+interface BookI {
+  isbn: string;
+  title: string;
+  rating: number;
+}
+
+function rateUp(book: BookI): BookI {
+  book.rating++;
+  return book;
+}
+
+
+const myBook: BookI = {
+  isbn: '123',
+  title: 'Angular',
+  rating: 5
+};
