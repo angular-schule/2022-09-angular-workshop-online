8a5d4481910a61d08958cb1337ff6e01f5e435d8
Ferdinand Malcher <ferdinand@malcher.media>
2022-09-19 11:36:10 +0200

    Vorbereitung Formulare und Suche

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 // import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
